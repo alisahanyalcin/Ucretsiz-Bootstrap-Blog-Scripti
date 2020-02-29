f27e7b38db80393ba5e85479a58cc6a32d226e1d
Ali Şahan Yalçın <34830846+alisahanyalcin@users.noreply.github.com>
2020-02-29 23:47:32 +0300

    Update ucretsizblog.sql

--- ucretsizblog.sql
+++ ucretsizblog.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Anamakine: 127.0.0.1
--- Üretim Zamanı: 29 Şub 2020, 18:07:18
--- Sunucu sürümü: 10.4.11-MariaDB
--- PHP Sürümü: 7.4.1
+-- Anamakine: localhost:3306
+-- Üretim Zamanı: 29 Şub 2020, 23:45:50
+-- Sunucu sürümü: 10.1.42-MariaDB
+-- PHP Sürümü: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -54,9 +54,9 @@ CREATE TABLE `blog` (
   `gorsel` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `kategori` varchar(128) COLLATE utf8_turkish_ci NOT NULL,
   `ozet` text COLLATE utf8_turkish_ci NOT NULL,
-  `icerik` text COLLATE utf8_turkish_ci DEFAULT NULL,
+  `icerik` text COLLATE utf8_turkish_ci,
   `tarih` varchar(255) COLLATE utf8_turkish_ci NOT NULL,
-  `aciklama` text COLLATE utf8_turkish_ci DEFAULT NULL,
+  `aciklama` text COLLATE utf8_turkish_ci,
   `anahtar_kelimeler` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `aktiflik` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
@@ -68,7 +68,7 @@ CREATE TABLE `blog` (
 INSERT INTO `blog` (`id`, `adi`, `link`, `gorsel`, `kategori`, `ozet`, `icerik`, `tarih`, `aciklama`, `anahtar_kelimeler`, `aktiflik`) VALUES
 (29, 'What is Lorem Ipsum?', 'what-is-lorem-ipsum', 'lorem-ipsum.webp', '6', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '<h2><strong>What is Lorem Ipsum?</strong></h2>\r\n\r\n<p><strong>Lorem Ipsum</strong> is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry&#39;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>\r\n\r\n<h2><strong>Why do we use it?</strong></h2>\r\n\r\n<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using &#39;Content here, content here&#39;, making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for &#39;lorem ipsum&#39; will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>\r\n', '29-02-2020 18:16', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', 'Lorem Ipsum, Lipsum, Lorem, Ipsum, Text, Generate, Generator, Facts, Information, What, Why, Where, Dummy Text, Typesetting, Printing, de Finibus, Bonorum et Malorum, de Finibus Bonorum et Malorum, Extremes of Good and Evil, Cicero, Latin, Garbled, Scramb', 1),
 (30, 'Bootstrap Nedir?', 'bootstrap-nedir', 'bootstrap.jpg', '6', 'Ücretsiz bir CSS framework’u olan Bootstrap, açık kaynak kodlu bir tasarım aracıdır.', '<h1><strong>bootstrap nedir?</strong></h1>\r\n\r\n<p>Ücretsiz bir CSS framework’u olan Bootstrap, açık kaynak kodlu bir tasarım aracıdır. Telefon, tablet ve masaüstü bilgisayarlar için farklı ve cihazınızın büyüklüğüyle orantılı şekilde sitenizin görünmesini sağlayan tema ve tasarımları kolaylıkla yapabilirsiniz. Bir site için gerekli olan bütün unsurları içerisinde barındıran Bootstrap ile tasarım yaparken bu hazır unsurları kullanarak tüm cihazlara uygun tasarımlar yapmanızı sağlar. Bu da demek oluyor ki, her şeyi hazır olan kodlarla yeni bir tasarım yaratmak oldukça kolay ve pratiktir. Stiller, imajlar ve JavaScript&#39;ler daha öncesinden Bootstrap&#39;in içine monte edilmiştir. Yapmanız gereken tek şey bunları çağırmanızdır.</p>\r\n', '29-02-2020 18:51', 'Ücretsiz bir CSS framework’u olan Bootstrap, açık kaynak kodlu bir tasarım aracıdır.', 'bootstrap, bootstrap nedir', 1),
-(31, 'REST API Nedir?', 'rest-api-nedir', 'rest-api.png', '1', 'REST API Nedir?', '<h1><strong>REST API Nedir?</strong></h1>\r\n\r\n<p><strong>REST API</strong>, geliştiricilerin HTTP protokolünü kullanarak GET ve POST gibi isteklerde bulunup, bu isteklere çeşitli formatlarda yanıt aldığı bir dağıtık sistemdir. REST (REpresentational State Transfer), Temsili Durum Aktarımı olarak Türkçede de kullanılır. REST’in tüm prensiplerini yerine getiren API’ler ise RESTful olarak nitelendirilir. REST API ilk defa 2000 yılında Roy Fielding tarafından doktora tezinin bir bölümü olarak geliştirilmiş ve ardından geliştiricilerce kabul görmüştür. REST API, web mimarisi kısmında çalışsın veya çalışmasın her geliştiricinin birgün ihtiyaç duyabileceği popüler bir API’dir.</p>\r\n\r\n<p>REST API, gelişticilerin hakim olduğu SOAP ve WSDL tabanlı web servislerine benzer ancak daha basite indirgenerek geliştirilmiştir. REST API kullanılırken web tarayıcılarının sayfa transfer sürecini bir parçası olan HTTP fiilleri kullanılarak haberleşilir. Dolayısıyla geliştirici olarak size tanıdık gelen GET, POST, PUT DELETE vb. filler REST API ile haberleşmede oldukça yaygın kullanılır. HTTP protokolünü kullanıyor olması nedeniyle REST API’i pratik olarak herhangi bir programlama dilinde kullanabilir. REST API’yi yazılımlarınızda kullanarak yazılımınızın uzun ömürlü olmasını sağlayabilir ve gelişimini pozitif yönde destekleyebilirsiniz.</p>\r\n', '29-02-2020 18:56', 'REST API Nedir?', 'rest api, rest api nedir?', 1);
+(31, 'REST API Nedir?', 'rest-api-nedir', 'rest-api.png', '1', 'REST API Nedir?', '<h1><strong>REST API Nedir?</strong></h1>\r\n\r\n<p><strong>REST API</strong>, geliştiricilerin HTTP protokolünü kullanarak GET ve POST gibi isteklerde bulunup, bu isteklere çeşitli formatlarda yanıt aldığı bir dağıtık sistemdir. REST (REpresentational State Transfer), Temsili Durum Aktarımı olarak Türkçede de kullanılır. REST’in tüm prensiplerini yerine getiren API’ler ise RESTful olarak nitelendirilir. REST API ilk defa 2000 yılında Roy Fielding tarafından doktora tezinin bir bölümü olarak geliştirilmiş ve ardından geliştiricilerce kabul görmüştür. REST API, web mimarisi kısmında çalışsın veya çalışmasın her geliştiricinin birgün ihtiyaç duyabileceği popüler bir API’dir.</p>\r\n\r\n<p>REST API, gelişticilerin hakim olduğu SOAP ve WSDL tabanlı web servislerine benzer ancak daha basite indirgenerek geliştirilmiştir. REST API kullanılırken web tarayıcılarının sayfa transfer sürecini bir parçası olan HTTP fiilleri kullanılarak haberleşilir. Dolayısıyla geliştirici olarak size tanıdık gelen GET, POST, PUT DELETE vb. filler REST API ile haberleşmede oldukça yaygın kullanılır. HTTP protokolünü kullanıyor olması nedeniyle REST API’i pratik olarak herhangi bir programlama dilinde kullanabilir. REST API’yi yazılımlarınızda kullanarak yazılımınızın uzun ömürlü olmasını sağlayabilir ve gelişimini pozitif yönde destekleyebilirsiniz.</p>\r\n', '29-02-2020 18:56', 'REST API Nedir?', 'rest api, rest api nedir?', 0);
 
 -- --------------------------------------------------------
 
@@ -90,7 +90,8 @@ CREATE TABLE `iletisim` (
 --
 
 INSERT INTO `iletisim` (`id`, `isim_soyisim`, `mail`, `mesaj`, `tarih`, `ip`) VALUES
-(9, 'Alişahan yalçın', 'alisahanyalcin4@gmail.com', 'advaddv', '29.02.2020 17:50', '::1');
+(9, 'Alişahan yalçın', 'alisahanyalcin4@gmail.com', 'advaddv', '29.02.2020 17:50', '::1'),
+(10, 'alisahanyalcin', 'alisahanyalcin4@gmail.com', 'deneme iletişim mesajı', '29.02.2020 20:30', 'yok');
 
 -- --------------------------------------------------------
 
@@ -134,9 +135,9 @@ CREATE TABLE `menu` (
 --
 
 INSERT INTO `menu` (`id`, `sira`, `yazi`, `link`, `aktiflik`) VALUES
-(1, 1, 'Anasayfa', 'http://localhost/ucretsiz/blog/', 1),
-(2, 2, 'Hakkımızda', 'http://localhost/ucretsiz/blog/sayfa/hakkimizda', 1),
-(4, 3, 'İletişim', 'http://localhost/ucretsiz/blog/iletisim', 1);
+(1, 1, 'Anasayfa', 'https://demo.alisahanyalcin.com/ucretsizblog/', 1),
+(2, 2, 'Hakkımızda', 'https://demo.alisahanyalcin.com/ucretsizblog/sayfa/hakkimizda', 1),
+(4, 3, 'İletişim', 'https://demo.alisahanyalcin.com/ucretsizblog/iletisim', 1);
 
 -- --------------------------------------------------------
 
@@ -212,8 +213,8 @@ CREATE TABLE `yorumlar` (
 --
 
 INSERT INTO `yorumlar` (`id`, `blog_id`, `profil`, `ad`, `mail`, `yorum`, `durum`, `tarih`, `ip`) VALUES
-(1, 26, '5.png', 'Alişahan yalçın', 'alisahanyalcin4@gmail.com', 'deneme', 1, '29.02.2020 16:04', '::1'),
-(2, 28, '5.png', 'Alişahan yalçın', 'alisahanyalcin4@gmail.com', 'Deneme Blog Yorumu', 1, '29.02.2020 17:22', '::1');
+(3, 31, '1.png', 'Ali Şahan Yalçın', 'alisahanyalcin4@gmail.com', 'deneme yorum', 1, '29.02.2020 20:24', 'yok'),
+(4, 31, '9.png', 'Yorumcu', 'yorumcu@mail.com', 'Deneme yorum 2', 1, '29.02.2020 20:28', 'yok');
 
 --
 -- Dökümü yapılmış tablolar için indeksler
@@ -288,7 +289,7 @@ ALTER TABLE `blog`
 -- Tablo için AUTO_INCREMENT değeri `iletisim`
 --
 ALTER TABLE `iletisim`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `kategoriler`
@@ -300,7 +301,7 @@ ALTER TABLE `kategoriler`
 -- Tablo için AUTO_INCREMENT değeri `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `sayfalar`
@@ -318,7 +319,7 @@ ALTER TABLE `sistem_ayarlari`
 -- Tablo için AUTO_INCREMENT değeri `yorumlar`
 --
 ALTER TABLE `yorumlar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
