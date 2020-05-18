889eff7984d887f118e7ae029e38db2d96241997
Ali Şahan Yalçın <34830846+alisahanyalcin@users.noreply.github.com>
2020-05-18 21:18:45 +0300

    add google recaptcha config

---
+++ application/config/recaptcha.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['recaptcha_site_key'] = '';
+$config['recaptcha_secret_key'] = '';
+$config['recaptcha_lang'] = 'tr';
