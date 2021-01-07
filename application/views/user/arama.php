184f242ec366b8ea8b5fb5c1b216d2bfdd3049e0
Ali Şahan Yalçın <34830846+alisahanyalcin@users.noreply.github.com>
2021-01-07 14:13:48 +0300

    Update arama.php

--- application/views/user/arama.php
+++ application/views/user/arama.php
@@ -1,26 +1,26 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8">
-            <?php if(is_array($results) && count($results) > 0):
-                foreach ($results as $rows) :?>
+            <?php if(is_array($result)):
+                foreach ($result as $rows) :?>
                     <div class="card">
-                        <img class="card-img-top" src="<?= base_url()."assets/upload/".$rows->gorsel;?>" alt="<?= $rows->adi;?>">
+                        <img class="card-img-top" src="<?= base_url()."assets/upload/".$rows['gorsel'];?>" alt="<?= $rows['adi'];?>">
                         <div class="card-body">
-                            <h5 class="card-title"><?= $rows->adi;?></h5>
+                            <h5 class="card-title"><?= $rows['adi'];?></h5>
                             <p class="card-text">
-                                <?= $rows->ozet;?>
+                                <?= $rows['ozet'];?>
                             </p>
-                            <a href="<?= base_url().$rows->link;?>" class="btn btn-primary">Devamını Oku</a>
+                            <a href="<?= base_url().$rows['link'];?>" class="btn btn-primary">Devamını Oku</a>
                         </div>
                         <div class="card-footer text-muted">
-                            <?= time_ago($rows->tarih);?>, <a href="<?= base_url()."kategori/".$this->UserGetModel->getIdKatLink($rows->kategori);?>"><?= $this->UserGetModel->getIdKategori($rows->kategori);?></a>
+                            <?= time_ago($rows['tarih']);?>, <a href="<?= base_url()."kategori/".$this->UserGetModel->getIdKatLink($rows['kategori']);?>"><?= $this->UserGetModel->getIdKategori($rows['kategori']);?></a>
                         </div>
                     </div>
                 <?php
                 endforeach;
             else: ?>
                 <div class="alert alert-warning" role="alert">
-                    <?=$kelime;?> kelimesi ile eşleşen hiç b,r sonuç bulunamadı. Farklı bir anahtar kelime ile arama yapabilir ya da <a href="<?= base_url();?>">Anasayfaya</a> dönebilirsiniz.
+                    <?=$kelime;?> kelimesi ile eşleşen hiç bir sonuç bulunamadı. Farklı bir anahtar kelime ile arama yapabilir ya da <a href="<?= base_url();?>">Anasayfaya</a> dönebilirsiniz.
                 </div>
             <?php endif;?>
         </div>
@@ -34,7 +34,7 @@
                         <div class="input-group">
                             <input type="text" class="form-control" name="s" placeholder="Ara..." value="<?=$kelime;?>">
                             <span class="input-group-btn">
-                                    <button class="btn btn-secondary" type="submit"><i class="las la-search"></i></button>
+                                    <button class="btn btn-primary" type="submit"><i class="las la-search"></i></button>
                                 </span>
                         </div>
                     </form>
